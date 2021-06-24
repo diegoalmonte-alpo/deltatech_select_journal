85896a000ec8f83e5e38548ed61b9f7fa11cdc54
diegoalmonte-alpo <62121801+diegoalmonte-alpo@users.noreply.github.com>
2021-06-24 15:05:19 -0400

    Add files via upload

---
+++ models/account_payment.py
@@ -0,0 +1,26 @@
+# ©  2015-2020 Terrabit
+# See README.rst file on addons root folder for license details
+
+from odoo import models
+
+
+class AccountPayment(models.Model):
+    _inherit = "account.payment"
+
+    # inutil, se defineste o secventa speciala pentru numerar
+    # @api.multi
+    # def post(self):
+    #     """ Mostenire functie post pentru selectarea seriei.
+    #     seria standard ar trebui dezactivata
+    #     """
+    #
+    #     for rec in self:
+    #         if rec.partner_type == "customer":
+    #             if not rec.name and rec.payment_type != "transfer":
+    #                 if rec.journal_id:
+    #                     rec.sudo().journal_id.sequence_id.number_next = (
+    #                         rec.journal_id.sequence_id.number_next - 1
+    #                     )  # conflicts with account.move sequence
+    #                     rec.name = rec.journal_id.sequence_id.next_by_id()
+    #                     pass
+    #     return super(AccountPayment, self).post()
